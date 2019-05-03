cfda8d1f715bddf3eba11040f6f4e4c5cc3212cb
dan-rds <ddrichar@ucsc.edu>
2019-05-03 16:04:54 -0700

    camera working

--- app/src/main/java/com/example/snapalarm/CameraActivity.java
+++ app/src/main/java/com/example/snapalarm/CameraActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -25,35 +27,32 @@ public class CameraActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
+        Log.d("___onCreate Camera", "created");
         dispatchTakePictureIntent();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        if (resultCode == RESULT_OK) {
-            Bundle extras = takePictureIntent.getExtras();
-            Log.d("___Bundle picIntent", extras.toString());
+        Log.d("___onResult Camera", String.valueOf(requestCode));
 
+        String toastMsg;
+        if (resultCode == -1 ) {
+            toastMsg = "Picture Taken!";
         }
+        else {
+            toastMsg = "Picture NOT Taken!";
+        }
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        i.putExtra("camera_msg", toastMsg);
+        startActivity(i);
     }
 
 
 
     private void dispatchTakePictureIntent() {
+        Log.d("___Camera", "dispatchTakePictureIntent");
         takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(takePictureIntent, REQUEST_CODE);
-
-
-
     }
-
-//    private void galleryAddPic() {
-//        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-//        File f = new File(mCurrentPhotoPath);
-//        Uri contentUri = Uri.fromFile(f);
-//        mediaScanIntent.setData(contentUri);
-//        this.sendBroadcast(mediaScanIntent);
-//    }
 
 }
--- app/src/main/java/com/example/snapalarm/MainActivity.java
+++ app/src/main/java/com/example/snapalarm/MainActivity.java
@@ -7,9 +7,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final Context context = this;
@@ -38,6 +40,21 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        Log.d("______Main", "resumed");
+
+        try {
+            Bundle extras = getIntent().getExtras();
+            if (extras.containsKey("camera_msg")){
+                Toast.makeText(getApplicationContext(), extras.getCharSequence("camera_msg"), Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Log.e("__main onResume", e.toString());
+        }
     }
 
     @Override
