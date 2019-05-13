f60cab4d5099ebcf98edc8bf90abdad3e5e5e73a
Chris Mendoza <chesmend@ucsc.edu>
2019-05-13 11:21:36 -0700

    Added funtionality for the AlarmModel

--- app/src/main/java/com/example/snapalarm/AlarmModel.java
+++ app/src/main/java/com/example/snapalarm/AlarmModel.java
@@ -4,18 +4,72 @@ package com.example.snapalarm;
 import java.util.List;
 
 public class AlarmModel {
-    private float _alarmTimeUTC;
-    private List <Integer> _repeatDays;
-    // TODO figure out the sound type
-   // private sound alarmsound
+    private String hour;
+    private String min;
+    private String ampm;
+    private String sun;
+    private String mon;
+    private String tue;
+    private String wed;
+    private String thu;
+    private String fri;
+    private String sat;
 
-    AlarmModel(float time, List<Integer> repeatDays){
-        this._alarmTimeUTC = time;
-        this._repeatDays = repeatDays;
+    public AlarmModel() {
+
     }
-    public float getTime(){
-        return this._alarmTimeUTC;
+
+    public AlarmModel(String hour, String min, String ampm, String sun, String mon,
+                      String tue, String wed, String thu, String fri, String sat) {
+        this.hour = hour;
+        this.min = min;
+        this.min = ampm;
+        this.min = sun;
+        this.min = mon;
+        this.min = tue;
+        this.min = wed;
+        this.min = thu;
+        this.min = fri;
+        this.min = sat;
     }
 
+    public void setHour(String hour) { this.hour = hour; }
 
+    public void setMin(String min) { this.min = min; }
+
+    public void setAmpm(String ampm) { this.ampm = ampm; }
+
+    public void setSun(String sun) { this.sun = sun; }
+
+    public void setMon(String mon) { this.mon = mon; }
+
+    public void setTue(String tue) { this.tue = tue; }
+
+    public void setWed(String wed) { this.wed = wed; }
+
+    public void setThu(String thu) { this.thu = thu; }
+
+    public void setFri(String fri) { this.fri = fri; }
+
+    public void setSat(String sat) { this.sat = sat; }
+
+    public String getHour() { return hour; }
+
+    public String getMin() { return min; }
+
+    public String getAmpm() { return ampm; }
+
+    public String getSun() { return sun; }
+
+    public String getMon() { return mon; }
+
+    public String getTue() { return tue; }
+
+    public String getWed() { return wed; }
+
+    public String getThu() { return thu; }
+
+    public String getFri() { return fri; }
+
+    public String getSat() { return sat; }
 }
--- app/src/main/java/com/example/snapalarm/DatabaseHelper.java
+++ app/src/main/java/com/example/snapalarm/DatabaseHelper.java
@@ -15,16 +15,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String keyHour = "hours";
     private static final String keyMin = "mins";
     private static final String keyAMPM = "ampm";
+    private static final String keySun = "sun";
+    private static final String keyMon = "mon";
+    private static final String keyTue = "tue";
+    private static final String keyWed = "wed";
+    private static final String keyThu = "thu";
+    private static final String keyFri = "fri";
+    private static final String keySat = "sat";
 
     public DatabaseHelper(Context context) { super(context, dbName, null, 1); }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onConfirgue(SQLiteDatabase db) {
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String createAlarmTable = "Create Table " + tableAlarms + " (" + keyHour + " TEXT, " +
+                                  keyMin + " TEXT, " + keyAMPM + " TEXT, " + keySun + " TEXT, " +
+                                  keyMon + " TEXT, " + keyTue + " TEXT, " + keyWed + " TEXT, " +
+                                  keyThu + " TEXT, " + keyFri + " TEXT, " + keySat + " TEXT)";
+        db.execSQL(createAlarmTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + tableAlarms);
+        onCreate(db);
+    }
 
+    // Adding an alarm
+    public void addAlarm(AlarmModel alarmModel) {
+        SQLiteDatabase db = 
     }
 }
