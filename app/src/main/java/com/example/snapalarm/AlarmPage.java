8eea8df84f943711193b1d37a2e3355e0289e248
Kalyn Williams <43970775+Kaswilli@users.noreply.github.com>
2019-05-16 20:03:03 -0700

    UI
    I did some more work to the ui. I hope to chage the background based on the day and do some more changes.

--- app/src/main/java/com/example/snapalarm/AlarmPage.java
+++ app/src/main/java/com/example/snapalarm/AlarmPage.java
@@ -1,11 +1,19 @@
 package com.example.snapalarm;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 public class AlarmPage extends AppCompatActivity {
@@ -16,6 +24,10 @@ public class AlarmPage extends AppCompatActivity {
             "32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47",
             "48","49","50","51","52","53","54","55","56","57","58","59"};
     String[] ampm={"AM","PM"};
+    RelativeLayout relativeLayout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
