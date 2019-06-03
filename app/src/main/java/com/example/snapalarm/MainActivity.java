9a3e1bf4a820ceb3bf116d3a65458cef63a561b0
Kalyn Williams <43970775+Kaswilli@users.noreply.github.com>
2019-06-03 12:41:58 -0700

    Update MainActivity.java
    New UI design

--- app/src/main/java/com/example/snapalarm/MainActivity.java
+++ app/src/main/java/com/example/snapalarm/MainActivity.java
@@ -6,8 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
